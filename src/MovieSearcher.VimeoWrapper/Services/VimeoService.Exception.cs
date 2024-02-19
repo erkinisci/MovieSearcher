3b85794720b4fc558fa8f58c7dc7b317310828c3
E. ISCI <erkin.isci@gmail.com>
2024-02-19 22:14:14 +0100

    refactor: add a comment for method

--- src/MovieSearcher.VimeoWrapper/Services/VimeoService.Exception.cs
+++ src/MovieSearcher.VimeoWrapper/Services/VimeoService.Exception.cs
@@ -6,8 +6,8 @@ public partial class VimeoService
     /// https://restfulapi.net/http-status-codes/
     /// A 500 error is never the client’s fault, and therefore, it is reasonable for the client to retry the same request that triggered this response and hope to get a different response.
     /// </summary>
-    /// <param name="exception"></param>
-    /// <returns></returns>
+    /// <param name="exception">Exception</param>
+    /// <returns>Boolean</returns>
     private static bool ExceptionChecker(Exception exception)
     {
         return exception.Message.Contains("Internal Server Error", StringComparison.OrdinalIgnoreCase);

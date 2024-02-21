d937abc7ba63517c8d190ea5461ed82d9f9b57dd
E. ISCI <erkin.isci@gmail.com>
2024-02-21 18:40:21 +0100

    feat: export method into interface

--- src/MovieSearcher.Core/Utility/IDelayService.cs
+++ src/MovieSearcher.Core/Utility/IDelayService.cs
@@ -3,4 +3,5 @@ namespace MovieSearcher.Core.Utility;
 public interface IDelayService
 {
     Task DelayWithExponentialBackoff(int failedAttempts, int maxDelayInSeconds = 1024);
+    int CalculateExponentialBackoffDelay(int failedAttempts, int maxDelayInSeconds = 1024);
 }
